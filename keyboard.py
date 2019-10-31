ca7f0c7117d481efcdb00379bc1c8a6315b906bb
Varad More <38086379+varad-more@users.noreply.github.com>
2019-11-01 01:49:16 +0530

    Fianl Key Controlled

--- keyboard.py
+++ keyboard.py
@@ -8,6 +8,22 @@ tty.setcbreak(sys.stdin)
 x = 0
 while x != chr(27): # ESC
     x=sys.stdin.read(1)[0]
-    print("You pressed", x)
+    if x=="a":
+        print ("Left")
+    elif x=="d":
+        print ("Right")
+    elif x=="s":
+        print ("Down")
+    elif x=="w":
+        print ("Up")
+    elif x=="i":
+        print ("Throttle Inc")
+    elif x=="k":
+        print ("Throttle Dec")
+    else:
+        print ("Invalid")
+
+
+  #  print("You pressed", x)
 
 termios.tcsetattr(sys.stdin, termios.TCSADRAIN, orig_settings)    
